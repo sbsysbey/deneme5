158b48c6c8b33241dcb734ffe3fea663a789c9db
S.Koru <skoru1972@gmail.com>
2020-11-20 18:01:06 +0100

    sr1 den ilk

--- src/deneme5.java
+++ src/deneme5.java
@@ -3,6 +3,12 @@ public class deneme5 {
     public static void main(String[] args) {
 
         System.out.println("denemeler" );
+        System.out.println("buda branch a yeni kopy" );
+        System.out.println("bir daha" );
+
+
+
+
 
             }
 
